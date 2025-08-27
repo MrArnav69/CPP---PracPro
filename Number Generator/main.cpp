cf718b48538146bab03e5e0cf866616854104cbc
MrArnav69 <arnavgupta6969@icloud.com>
2025-08-27 11:02:04 +0545

    New pr

--- Number Generator/main.cpp
+++ Number Generator/main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    
+    //Simple Number Generator 1-100
+    /*
+     Uses basic For Loop for number generation
+     */
     for(int i =1; i<=100; i++){
         
         cout<<"The number is "<<i<<endl;
---
+++ Student Grade Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Student Grade Calculator
+//
+//  Created by Arnav Gupta on 27/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
